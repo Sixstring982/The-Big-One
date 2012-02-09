f2ba1aa6d229bf3192d2201e355f84b28765cb26
Trent Small <Sixstring982@gmail.com>
2012-02-09 12:24:11 -0700

    First Commit

---
+++ README.txt
@@ -0,0 +1,22 @@
+The Big One --
+
+Random room based dungeon crawler
+Themed as a spy walking through a museum
+Get treasure, avoid traps, use gadgets
+to disarm traps.
+
+Treasure gets randomly generated name
+
+A "twist" at the end of each level, proceeding
+to the next floor.
+
+After all lives are used, the game is over.
+
+Janitor's closet room contains items to
+deactivate traps
+Janitor is an NPC (Trader)
+
+Trap ideas --
+
+Pressure Plate - deactivated by deadweight.
+Lasers - Spray can?
---
+++ The Big One/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace The_Big_One
+{
+    class Program
+    {
+        static bool gameOver = false;
+        static void Main(string[] args)
+        {
+            ShowSplash();
+
+            while (!gameOver)
+            {
+                DoInput(ReadInput());
+                break;
+            }
+            Console.ReadKey();
+        }
+
+        static string[] ReadInput()
+        {
+            return Console.ReadLine().Split(" ".ToCharArray());
+        }
+
+        static void DoInput(string[] inputString)
+        {
+            switch(inputString[0].ToLower())
+            {
+                case "n":
+                    break;
+            }
+        }
+
+        static void ShowSplash()
+        {
+            Console.WriteLine("The Big One");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("©2012 Trent Small");
+            Console.WriteLine("     Zaach Tubb");
+            Console.WriteLine("");
+        }
+    }
+}
